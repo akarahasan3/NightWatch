ee189d62e36fdae5d62ccc31a856692615094dea
Ajdin Karahasanovic <ajdin.ka1@gmail.com>
2023-07-14 09:40:07 +0200

    Initial commit

---
+++ NightWatch/NightWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NightWatch
+//
+//  Created by Ajdin Karahasanovic on 14.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
